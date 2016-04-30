6fa912292b36c6ac0d8810e748347da11be28f3e
Marcel Spindler <marcelspindler1@marcels-macbook.fritz.box>
2016-04-30 17:19:28 +0200

    UebungenAnzeigenVC erstellt und UebungenAnzeigenVC mit MuskelgruppenVC im Storyboard verbunden

---
+++ Pump4/UebungenAnzeigenViewController.swift
@@ -0,0 +1,14 @@
+//
+//  UebungenAnzeigenViewController.swift
+//  Pump4
+//
+//  Created by Marcel Spindler on 30.04.16.
+//  Copyright © 2016 Marcel Spindler. All rights reserved.
+//
+
+import UIKit
+
+class UebungenAnzeigenViewController: UIViewController {
+
+
+}
---
+++ Pump4/UebungenMuskelgruppenTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  UebungenMuskelgruppenTableViewController.swift
+//  Pump4
+//
+//  Created by Marcel Spindler on 30.04.16.
+//  Copyright © 2016 Marcel Spindler. All rights reserved.
+//
+
+import UIKit
+
+class UebungenMuskelgruppenTableViewController: UITableViewController {
+
+    
+}
