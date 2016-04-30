b6f9eaaa85bd8febdf14378be661dfda1d22597b
Marcel Spindler <marcelspindler1@marcels-macbook.fritz.box>
2016-04-30 17:02:24 +0200

    TrainingAuswahlVC hinzugefügt mit navigationcontroller und back barbutton

---
+++ Pump4/TrainingAuswahlViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingAuswahlViewController.swift
+//  Pump4
+//
+//  Created by Marcel Spindler on 30.04.16.
+//  Copyright © 2016 Marcel Spindler. All rights reserved.
+//
+
+import UIKit
+
+class TrainingAuswahlViewController: UIViewController {
+
+    @IBAction func backButtonAction(sender: UIBarButtonItem) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
