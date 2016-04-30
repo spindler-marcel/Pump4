5335ca80f5e4a89e6a15c5b1ae52d31fdec450bb
Marcel Spindler <marcelspindler1@marcels-macbook.fritz.box>
2016-04-30 18:27:21 +0200

    Weitere Storyboardänderungen

---
+++ Pump4/TrainingsplanAnlegenViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TrainingsplanAnlegenViewController.swift
+//  Pump4
+//
+//  Created by Marcel Spindler on 30.04.16.
+//  Copyright © 2016 Marcel Spindler. All rights reserved.
+//
+
+import UIKit
+
+class TrainingsplanAnlegenViewController: UIViewController {
+
+    @IBOutlet weak var planNameTextField: UITextField!
+
+    @IBOutlet weak var fehlerLabel: UILabel!
+    @IBAction func weiterButtonAction(sender: UIButton) {
+        if planNameTextField.text!.isEmpty {
+            fehlerLabel.hidden = false
+        }
+        else{
+            
+        }
+
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        fehlerLabel.hidden = true
+    }
+}
---
+++ Pump4/TrainingsplanMenuViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TrainingsplanMenuViewController.swift
+//  Pump4
+//
+//  Created by Marcel Spindler on 30.04.16.
+//  Copyright © 2016 Marcel Spindler. All rights reserved.
+//
+
+import UIKit
+
+class TrainingsplanMenuViewController: UIViewController {
+
+}
